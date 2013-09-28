e882644fe4638b72e382864260f7d413b7578eae
macchicken <renault194@hotmail.com>
2013-09-28 13:17:29 +0800

    add interpreter change

--- basicPrac/matchset.py
+++ basicPrac/matchset.py
@@ -171,9 +171,54 @@ def new_seq(x, y): return lambda text: set().union(*map(y, x(text)))
 def new_star(x): return lambda text: (set([text])|set(t2 for t1 in x(text) if t1 != text for t2 in new_star(x)(t1)))
 def new_plus(x): return lambda text: new_seq(x, new_star(x))(text)
 def new_alt(x,y): return lambda text: set(x(text))|set(y(text))
-def new_oneof(chars): return lambda text: set([t[1:]]) if (text and text[0] in chars) else null
-new_dot = lambda t: set([t[1:]]) if t else null
-new_eol = lambda t: set(['']) if t == '' else null
+def new_oneof(chars): return lambda text: set([text[1:]]) if (text and text[0] in chars) else null
+def new_opt(x):       return new_alt(new_lit(''), x) #opt(x) means that x is optional
+new_dot = lambda text: set([text[1:]]) if text else null
+new_eol = lambda text: set(['']) if text == '' else null
+null = frozenset([])
+
+def new_compiler_match(pattern, text):
+	"Match pattern against start of text; return longest match found or None."
+	remainders=pattern(text)
+	if remainders:
+		shortest = min(remainders, key=len)
+		return text[:len(text)-len(shortest)]
+
+def new_compile_search(pattern, text):
+    for i in range(len(text) or 1):
+        m = new_compiler_match(pattern, text[i:])
+        if m is not None: return m
+
+def test_compiler_match():
+	assert new_compiler_match(new_star(new_lit('a')), 'aaaaabbbaa') == 'aaaaa'
+	assert new_compiler_match(new_lit('hello'), 'hello how are you?') == 'hello'
+	assert new_compiler_match(new_lit('x'), 'hello how are you?') == None
+	assert new_compiler_match(new_oneof('xyz'), 'x**2 + y**2 = r**2') == 'x'
+	assert new_compiler_match(new_oneof('xyz'), '   x is here!') == None
+	assert new_compiler_match(new_star(new_lit('a')), 'aaabcd') == 'aaa'
+	assert new_compiler_match(new_lit('abc'), 'abc') == 'abc'
+	assert new_compiler_match(new_alt(new_lit('b'), new_lit('c')), 'ab') == None
+	assert new_compiler_match(new_alt(new_lit('b'), new_lit('a')), 'ab') == 'a'
+	assert new_compile_search(new_lit(''), '') == ''
+	assert new_compile_search(new_alt(new_lit('b'), new_lit('c')), 'ab') == 'b'
+	assert new_compile_search(new_star(new_alt(new_lit('a'), new_lit('b'))), 'ab') == 'ab'
+	assert new_compile_search(new_alt(new_lit('b'), new_lit('c')), 'ad') == None
+	assert new_lit('abc')('abcdef') == set(['def'])
+	assert (new_seq(new_lit('hi '), new_lit('there '))('hi there nice to meet you')== set(['nice to meet you']))
+	assert new_alt(new_lit('dog'), new_lit('cat'))('dog and cat') == set([' and cat'])
+	assert new_dot('am i missing something?') == set(['m i missing something?'])
+	assert new_dot('') == frozenset([])
+	assert new_oneof('a')('aabc123') == set(['abc123'])
+	assert new_oneof('abc')('babc123') == set(['abc123'])
+	assert new_oneof('abc')('dabc123') == frozenset([])
+	assert new_eol('') == set([''])
+	assert new_eol('not end of line') == frozenset([])
+	assert new_star(new_lit('hey'))('heyhey!') == set(['!', 'heyhey!', 'hey!'])
+	assert new_plus(new_lit('hey'))('heyhey!') == set(['!', 'hey!'])
+	assert new_opt(new_lit('hey'))('heyhey!') == set(['hey!', 'heyhey!'])
+	return 'compiler_match tests pass'
+
+print test_compiler_match()
 
 pat=new_lit('a')
 print pat('a string')
@@ -183,20 +228,72 @@ print pat2('aaaaab')
 # dis.dis(pat)
 g=new_alt(new_lit('a'),new_lit('b'))
 print g('abc')
+pat3=new_star(pat)
+print pat3('aaa')
 
-def new_compiler_match(pattern, text):
-	"Match pattern against start of text; return longest match found or None."
-	remainders=pattern(text)
-	if remainders:
-		shortest = min(remainders, key=len)
-		return text[:len(text)-len(shortest)]
 
-def test_compiler_match():
-    assert new_compiler_match(star(lit('a')), 'aaaaabbbaa') == 'aaaaa'
-    assert new_compiler_match(lit('hello'), 'hello how are you?') == 'hello'
-    assert new_compiler_match(lit('x'), 'hello how are you?') == None
-    assert new_compiler_match(oneof('xyz'), 'x**2 + y**2 = r**2') == 'x'
-    assert new_compiler_match(oneof('xyz'), '   x is here!') == None
-    return 'compiler_match tests pass'
+# --------------
+# User Instructions
+#
+# Complete the code for the compiler by completing the constructor
+# for the patterns alt(x, y) and oneof(chars). 
 
-print test_compiler_match()
+def interpreter_lit(s): set_s=set([s]);return lambda Ns: set_s if len(s) in Ns else null
+def interpreter_alt(x, y):      return lambda Ns: set(x(Ns))|set(y(Ns))
+def interpreter_star(x):        return lambda Ns: interpreter_opt(interpreter_plus(x))(Ns)
+def interpreter_plus(x):        return lambda Ns: genseq(x, interpreter_star(x), Ns,startx=1) #Tricky
+def interpreter_oneof(chars): set_chars=set(chars);return lambda Ns: set_chars if Ns and 1 in Ns else null
+def interpreter_seq(x, y):      return lambda Ns: genseq(x, y, Ns)
+def interpreter_opt(x):         return interpreter_alt(interpreter_epsilon, x)
+interpreter_dot = interpreter_oneof('?')    # You could expand the alphabet to more chars.
+interpreter_epsilon = interpreter_lit('')   # The pattern that matches the empty string.
+def infinite_genseq(x,y,Ns,startx=0):
+	Nss=range(startx,max(Ns)+1)
+	return set(m1+m2 for m1 in x(Nss) for m2 in y(Nss) if len(m1+m2) in Ns)
+def genseq(x,y,Ns,startx=0):
+	# generate all x to produce the length of x from giving possible size startx 
+	# and deduct the Ns from x to generate all length of y
+	# then do the generation of all y,finally concatenate x and y
+	if not Ns:return null
+	xmatches=x(set(range(startx,max(Ns)+1)))
+	Ns_x=set(len(m) for m in xmatches)
+	Ns_y=set(n-m for n in Ns for m in Ns_x if n-m>=0)
+	ymatches=y(Ns_y)
+	return set(m1+m2 for m1 in xmatches for m2 in ymatches if len(m1+m2) in Ns)
+null = frozenset([])
+
+def interpreter_test():
+    f = interpreter_lit('hello')
+    assert f(set([1, 2, 3, 4, 5])) == set(['hello'])
+    assert f(set([1, 2, 3, 4]))    == null 
+
+    g = interpreter_alt(interpreter_lit('hi'), interpreter_lit('bye'))
+    assert g(set([1, 2, 3, 4, 5, 6])) == set(['bye', 'hi']) 
+    assert g(set([1, 3, 5])) == set(['bye'])
+
+    h = interpreter_oneof('theseletters')
+    assert h(set([1, 2, 3])) == set(['t', 'h', 'e', 's', 'l', 'r'])
+    assert h(set([2, 3, 4])) == null
+    return 'interpreter tests pass'
+
+def test_gen():
+    def N(hi):return set(range(hi+1))
+    a, b, c = map(interpreter_lit, 'abc')
+    assert interpreter_star(interpreter_oneof('ab'))(N(2)) == set(['', 'a', 'aa', 'ab', 'ba', 'bb', 'b'])
+    assert (interpreter_seq(interpreter_star(a), interpreter_seq(interpreter_star(b), interpreter_star(c)))(set([4])) ==
+            set(['aaaa', 'aaab', 'aaac', 'aabb', 'aabc', 'aacc', 'abbb',
+                 'abbc', 'abcc', 'accc', 'bbbb', 'bbbc', 'bbcc', 'bccc', 'cccc']))
+    assert (interpreter_seq(interpreter_plus(a), interpreter_seq(interpreter_plus(b), interpreter_plus(c)))(set([5])) ==
+            set(['aaabc', 'aabbc', 'aabcc', 'abbbc', 'abbcc', 'abccc']))
+    assert (interpreter_seq(interpreter_oneof('bcfhrsm'), interpreter_lit('at'))(N(3)) ==
+            set(['bat', 'cat', 'fat', 'hat', 'mat', 'rat', 'sat']))
+    assert (interpreter_seq(interpreter_star(interpreter_alt(a, b)), interpreter_opt(c))(set([3])) ==
+            set(['aaa', 'aab', 'aac', 'aba', 'abb', 'abc', 'baa',
+                 'bab', 'bac', 'bba', 'bbb', 'bbc']))
+    assert interpreter_lit('hello')(set([5])) == set(['hello'])
+    assert interpreter_lit('hello')(set([4])) == set()
+    assert interpreter_lit('hello')(set([6])) == set()
+    return 'test_gen passes'
+
+print interpreter_test()
+print test_gen()
