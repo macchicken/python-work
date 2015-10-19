0394bdb2c0353f78afd8c4b06ca0bc85446a5507
Barry <log525lg100@gmail.com>
2015-10-19 16:02:08 +1100

    add sort comparator
    add sort comparator

--- COMP5416/assignment2/mjpegGenerator.py
+++ COMP5416/assignment2/mjpegGenerator.py
@@ -4,17 +4,28 @@ import time
 
 class ImgbinData:
 	
-	def __init__(self,imgLen,rawData):
+	def __init__(self,imgLen,rawData,fileName):
 		self.imgLen=imgLen
 		self.rawData=rawData
+		self.fileName=fileName
 	
 	def getImgLen(self):
 		return self.imgLen
 	
 	def getRawData(self):
 		return self.rawData
+		
+	def getFileName(self):
+		return self.fileName
+		
+	def __repr__(self):
+		return self.fileName
 
-
+def ImgbinDataCmp(imgb1,imgb2):
+	if imgb1.fileName<imgb2.fileName: return -1
+	elif imgb1.fileName==imgb2.fileName: return 0
+	else: return 1
+	
 def appendZeroToFive(str):
 	if len(str)>=5: return str
 	else: return appendZeroToFive('0'+str)
@@ -25,17 +36,19 @@ def toMjpeg(dirName="D:\\workspace\\python-work\\COMP5416\\assignment2\\test"):
 		with open(dirName+"\\"+fileName,"rb") as jpgfile:
 			data=jpgfile.read()
 			imglen=appendZeroToFive(str(os.path.getsize(dirName+"\\"+fileName)))
-			temp=ImgbinData(imglen,data)
+			temp=ImgbinData(imglen,data,fileName)
 			waitingData.append(temp)
-	time.sleep(3)
+	time.sleep(1)
+	waitingData.sort(cmp=ImgbinDataCmp)
+	time.sleep(1)
 	with open (dirName+"\\"+"result.Mjpeg","wb") as mjpegFile: 
 		for tdata in waitingData:
 			mjpegFile.write(tdata.getImgLen())
 			print "write file length %s" % tdata.getImgLen()
-			time.sleep(3)
+			time.sleep(1)
 			mjpegFile.write(tdata.getRawData())
 			print "write image bin data"
-			time.sleep(3)
+			time.sleep(1)
 			
 			
 if __name__ == '__main__':
