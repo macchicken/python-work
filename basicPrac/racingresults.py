2aedd9c0ff1735717047b309455c3e58dc441ca2
Barry <log525lg100@gmail.com>
2015-12-12 21:24:35 +1100

    add time and location saving in racingresults script

--- basicPrac/racingresults.py
+++ basicPrac/racingresults.py
@@ -5,14 +5,33 @@ import re,xlsxwriter
 
 baseUrl="www.racingresults.com.au"
 
+class Match:
+	def __init__(self,state,location,time):
+		self.state=state
+		self.location=location
+		self.time=time
+		self.racingMatches=[]
+	
+	def getState(self):
+		return self.state
+	def getLocation(self):
+		return self.location
+	def getTime(self):
+		return self.time
+	def getRacingMatches(self):
+		return self.racingMatches
+	def addRacingMatch(self,oneMatch):
+		self.racingMatches.append(oneMatch)
+
 class MatchData:
-	def __init__(self,position,onumber,horseBarrier,jockey,trainer,matchName):
+	def __init__(self,position,onumber,horseBarrier,jockey,trainer,matchName,match):
 		self.position=position
 		self.onumber=onumber
 		self.horseBarrier=horseBarrier
 		self.jockey=jockey
 		self.trainer=trainer
 		self.matchName=matchName
+		self.match=match
 	
 	def getPosition(self):
 		return self.position
@@ -26,12 +45,9 @@ class MatchData:
 		return self.trainer
 	def getMatchName(self):
 		return self.matchName
+	def getMatch(self):
+		return self.match
 
-
-def findtags(text):
-	if text is not None and text.strip()!='':
-		return re.findall(r'<\s*[a-zA-Z0-9]+\s*(?:\w+\s*=\s*"[^"]*"\s*)*\s*>',text)
-	else: return []
 
 def connectHttpFromDomain(domainName,requestUrl):
 	conn = HTTPConnection(domainName)
@@ -47,52 +63,70 @@ def connectHttpFromDomain(domainName,requestUrl):
 
 def getOneMatchData(onemFile,onemdate):
 	odata,ostatus,_,_,_=connectHttpFromDomain(baseUrl,onemdate)
-	result=findtags(odata)
-	resultURL=[]
-	for r in result:
-		if 'race' in r and '<a href=' in r and 'linkExternal' not in r:
-			tmpr=BeautifulSoup(r,'html.parser')
-			resultURL.append(tmpr.find('a')["href"])
+	result=BeautifulSoup(odata,'html.parser')
+	accmetings=result.find("div",attrs={"id": "accordion-meetings"})
+	contents=[cont for cont in accmetings.contents if cont!=u'\n']
+	i=0
+	wholeMatches=[]
+	while i<len(contents):
+		con=contents[i]
+		state=con.select("span.state")[0].string
+		location=con.select("span.location")[0].string
+		time=con.select("span.date")[0].string
+		onePlace=Match(state,location,time)
+		placeMatches=contents[i+1].select("a")
+		for pma in placeMatches:
+			onePlace.addRacingMatch(pma["href"])
+		wholeMatches.append(onePlace)
+		i=i+2
 	with open(onemFile+".txt","wb") as temp:
-		for rurl in resultURL: temp.write(rurl+"\n")
-	matchResult=[]
+		for wm in wholeMatches:
+			for rurl in wm.getRacingMatches():
+				temp.write(rurl+"\n")
 	matchexcelResult=[]
-	for onePlaceMatch in resultURL:
-		print "%s,%s" % (onemdate,onePlaceMatch)
-		onePlaceMatchName=onePlaceMatch.split('/')[-2:-1][0]
-		opdata,opstatus,_,_,_=connectHttpFromDomain(baseUrl,onePlaceMatch)
-		opsoup=BeautifulSoup(opdata,'html.parser')
-		players=opsoup.find_all("a", attrs={"class": "accordion-anchor"})
-		playersStr=""
-		for onep in players:
-			position=onep.select("span.position")[0].string
-			if position is None: position=onep.select("abbr")[0].string
-			else: position=position.replace("\n",'')
-			onumber=onep.select("span.number")[0].string
-			horseBarrier=onep.select("span.horse-barrier")[0].string
-			jockey=onep.select("span.jockey")[0].string
-			trainer=onep.select("span.trainer")[0].string
-			playersStr=playersStr+"Finish:%s N0:%s Horse(Barrier):%s Jockey:%s Trainer:%s\n" % (position,onumber,horseBarrier,jockey,trainer)
-			matchexcelResult.append(MatchData(position=position,onumber=onumber,horseBarrier=horseBarrier,jockey=jockey,trainer=trainer,matchName=onePlaceMatchName))
-		matchResult.append(onePlaceMatchName+"\n"+playersStr)
-	with open("result/"+onemFile+"-result.txt","wb") as temp:
-		for mr in matchResult: temp.write(mr)
+	for wm in wholeMatches:
+		state=wm.getState()
+		location=wm.getLocation()
+		time=wm.getTime()
+		racingMatches=wm.getRacingMatches()
+		for oneMatch in racingMatches:
+			print "%s,%s" % (onemdate,oneMatch)
+			onePlaceMatchName=oneMatch.split('/')[-2:-1][0]
+			opdata,opstatus,_,_,_=connectHttpFromDomain(baseUrl,oneMatch)
+			opsoup=BeautifulSoup(opdata,'html.parser')
+			players=opsoup.find_all("a", attrs={"class": "accordion-anchor"})
+			for onep in players:
+				position=onep.select("span.position")[0].string
+				if position is None: position=onep.select("abbr")[0].string
+				else: position=position.replace("\n",'')
+				onumber=onep.select("span.number")[0].string
+				horseBarrier=onep.select("span.horse-barrier")[0].string
+				jockey=onep.select("span.jockey")[0].string
+				trainer=onep.select("span.trainer")[0].string
+				matchexcelResult.append(MatchData(position=position,onumber=onumber,horseBarrier=horseBarrier,jockey=jockey,trainer=trainer,matchName=onePlaceMatchName,match=Match(state,location,time)))
 	workbook = xlsxwriter.Workbook("result/"+onemFile+'-result.xlsx')
 	worksheet = workbook.add_worksheet()
-	worksheet.write(0,0,"Match")
-	worksheet.write(0,1,"Finish")
-	worksheet.write(0,2,"N0")
-	worksheet.write(0,3,"Horse(Barrier)")
-	worksheet.write(0,4,"Jockey")
-	worksheet.write(0,5,"Trainer")
+	worksheet.write(0,0,"State")
+	worksheet.write(0,1,"Location")
+	worksheet.write(0,2,"Time")
+	worksheet.write(0,3,"Match")
+	worksheet.write(0,4,"Finish")
+	worksheet.write(0,5,"N0")
+	worksheet.write(0,6,"Horse(Barrier)")
+	worksheet.write(0,7,"Jockey")
+	worksheet.write(0,8,"Trainer")
 	row=1
 	for one in matchexcelResult:
-		worksheet.write(row,0,one.getMatchName())
-		worksheet.write(row,1,one.getPosition())
-		worksheet.write(row,2,one.getOnumber())
-		worksheet.write(row,3,one.getHorseBarrier())
-		worksheet.write(row,4,one.getJockey())
-		worksheet.write(row,5,one.getTrainer())
+		place=one.getMatch()
+		worksheet.write(row,0,place.getState())
+		worksheet.write(row,1,place.getLocation())
+		worksheet.write(row,2,place.getTime())
+		worksheet.write(row,3,one.getMatchName())
+		worksheet.write(row,4,one.getPosition())
+		worksheet.write(row,5,one.getOnumber())
+		worksheet.write(row,6,one.getHorseBarrier())
+		worksheet.write(row,7,one.getJockey())
+		worksheet.write(row,8,one.getTrainer())
 		row=row+1
 
 
