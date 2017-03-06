6cfdcc84798c5ba166715d3371cb2114808c66c8
Barry <log525lg100@gmail.com>
2017-03-07 09:16:09 +1100

    add imdb spider
    add imdb spider

---
+++ pureGrap/getAllalbum.py
@@ -0,0 +1,12 @@
+import os,sys
+
+
+if __name__ == '__main__':
+	myCookie=sys.argv[1]
+	with open("D:/troopar/model_list.txt","rb") as tempRead:
+		for line in tempRead:
+			newLine=line.strip("\n|' '").split()
+			modelUrl=newLine[0].strip()
+			modelName=newLine[1].strip()
+			commandCall='scrapy crawl beautylegmodelalbumlist -a murl="%s" -a mname="%s" -a mcookie="%s"' % (modelUrl,modelName,myCookie)
+			os.system(commandCall)
